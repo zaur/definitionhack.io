1ebc24e4e6f860647be7adac51e25b8963ffe869
techmeat <T5Kf8pY9w9yRVLs>
2021-09-25 21:07:43 +0300

    Add Drawer

--- pages/app.vue
+++ pages/app.vue
@@ -3,15 +3,21 @@
     <div class='h-container'>
       <h1>{{ content.title }}</h1>
       <div v-html='content.content' />
+      <Drawer />
     </div>
   </main>
 </template>
 
 <script>
+import Drawer from '@/components/shared/Drawer'
 import pages from '@/mixins/pages'
 
 export default {
   name: 'AppPage',
+
+  components: {
+    Drawer,
+  },
 
   mixins: [pages],
 }
