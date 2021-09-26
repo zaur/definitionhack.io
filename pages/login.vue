09a6cd57109b0700ef0ec8a59f3e78dcf5e92abc
techmeat <T5Kf8pY9w9yRVLs>
2021-09-26 10:37:01 +0300

    Fix login

--- pages/login.vue
+++ pages/login.vue
@@ -4,12 +4,31 @@
       <div class="container">
         <header>
           <div class="row">
-            <div id="alerts" class="col-xs-12 alert hide" role="alert"></div>
+            <div class="col-xs-12 alert hide" role="alert" id="alerts"></div>
             <div class="col-xs-offset-10 col-xs-2">
-              <span id="userDID" class="badge rounded-pill bg-secondary">Not Connected</span>
+              <span class="badge rounded-pill bg-secondary" id="userDID">Not Connected</span>
             </div>
           </div>
         </header>
+        <main class="container center">
+          <div class="row">
+            <div class="col-xs-12 col-lg-6">
+              <iframe
+                class="documentation"
+                src="https://developers.ceramic.network/build/javascript/quick-start/"
+                id="ceramic_docs"
+              >
+              </iframe>
+            </div>
+            <div class="col-xs-12 col-lg-6 text-right">
+              <iframe
+                class="documentation"
+                src="https://developers.idx.xyz/build/quick-start/"
+                id="idx_docs"
+              ></iframe>
+            </div>
+          </div>
+        </main>
 
         <div class='actions'>
           <ui-button id="bauth" :disabled='askingAuth' @click='askAuth'>
@@ -75,10 +94,87 @@ export default {
 
 <style>
 .container {
+  margin: 0 auto;
   text-align: center;
 }
 
 .actions {
   margin: 50px 0 0;
 }
+
+main {
+  max-width: 600px;
+}
+.logo {
+  height: 125px;
+  width: 125px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.hide {
+  display: none;
+  padding: 0;
+  margin: 0;
+}
+.center {
+  margin: 0 auto;
+}
+.loader {
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  vertical-align: text-bottom;
+  border: 0.25em solid currentColor;
+  border-right-color: transparent;
+  border-radius: 50%;
+  -webkit-animation: spin 1s linear infinite;
+  -moz-animation: spin 1s linear infinite;
+  animation: spin 1s linear infinite;
+  margin-right: 10px;
+}
+@-moz-keyframes spin {
+  100% {
+    -moz-transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+@keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+.alert {
+  margin-top: 15px;
+}
+.space-around {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+.ceramic-btn.active {
+  background-color: #f87237 !important;
+  color: white !important;
+}
+.idx-btn.active {
+  background-color: #6535ff !important;
+  color: white !important;
+}
+ul {
+  padding-left: 20px;
+}
+.documentation {
+  resize: both;
+  overflow: auto;
+  height: 200px;
+  width: 100%;
+  border: 1px solid transparent;
+  display: none;
+}
+
 </style>
