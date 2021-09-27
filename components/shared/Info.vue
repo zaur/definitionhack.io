6e0706eb3d8e0a532102b5d26acfb819e92e3220
techmeat <T5Kf8pY9w9yRVLs>
2021-09-27 16:07:24 +0300

    test server 4

--- components/shared/Info.vue
+++ components/shared/Info.vue
@@ -36,10 +36,10 @@
 
 <script>
 import { mapActions, mapState } from 'vuex'
-import { TileDocument } from '@ceramicnetwork/stream-tile'
-import Ceramic from '@ceramicnetwork/http-client'
+// import { TileDocument } from '@ceramicnetwork/stream-tile'
+// import Ceramic from '@ceramicnetwork/http-client'
 
-const ceramic = new Ceramic('https://ceramic-clay.3boxlabs.com')
+// const ceramic = new Ceramic('https://ceramic-clay.3boxlabs.com')
 
 export default {
   props: {
@@ -77,65 +77,65 @@ export default {
     ...mapActions('nft', ['fetchNFTs']),
 
     async addSector () {
-      this.isDataLoading = true
-      ceramic.did = window.did
-
-      const didNFT =
-        `did:nft:eip155:4_erc721:0xd132f6597e2b16e43f1a5ccd4956568a14e36cc5_${this.NFTNumber}`;
-
-      try {
-        const tile = await TileDocument.create(
-          ceramic,
-          null,
-          {controllers: [didNFT], deterministic: true}
-        )
-        await tile.update({
-          "user": window.did._id,
-          "timestamp": Date.now()
-        }, {controllers: [didNFT]})
-
-        const commits = await ceramic.loadStreamCommits(tile.id)
-        const lastCommitId = commits[commits.length - 1].cid
-
-        let userStreamContent
-        if (!window.userStreamDoc?.content?.nftRecords) {
-          userStreamContent = {nftRecords: []}
-        } else {
-          userStreamContent = window.userStreamDoc.content
-        }
-        const newNftRecord = {
-          "nft_token_id": this.NFTNumber,
-          "nft_stream_id": tile.id.toString(),
-          "commit_id": lastCommitId,
-          "timestamp": Date.now()
-        }
-        userStreamContent.nftRecords.push(newNftRecord)
-        await window.userStreamDoc.update(
-          userStreamContent,
-          {controllers: [window.did._id]}
-        )
-        await this.updateList()
-        this.isDataLoading = false
-        // todo update drawing state by window.userStreamDoc.content.nftRecords
-      } catch (error) {
-        console.error('ERROR', error)
-      }
+      // this.isDataLoading = true
+      // ceramic.did = window.did
+      //
+      // const didNFT =
+      //   `did:nft:eip155:4_erc721:0xd132f6597e2b16e43f1a5ccd4956568a14e36cc5_${this.NFTNumber}`;
+      //
+      // try {
+      //   const tile = await TileDocument.create(
+      //     ceramic,
+      //     null,
+      //     {controllers: [didNFT], deterministic: true}
+      //   )
+      //   await tile.update({
+      //     "user": window.did._id,
+      //     "timestamp": Date.now()
+      //   }, {controllers: [didNFT]})
+      //
+      //   const commits = await ceramic.loadStreamCommits(tile.id)
+      //   const lastCommitId = commits[commits.length - 1].cid
+      //
+      //   let userStreamContent
+      //   if (!window.userStreamDoc?.content?.nftRecords) {
+      //     userStreamContent = {nftRecords: []}
+      //   } else {
+      //     userStreamContent = window.userStreamDoc.content
+      //   }
+      //   const newNftRecord = {
+      //     "nft_token_id": this.NFTNumber,
+      //     "nft_stream_id": tile.id.toString(),
+      //     "commit_id": lastCommitId,
+      //     "timestamp": Date.now()
+      //   }
+      //   userStreamContent.nftRecords.push(newNftRecord)
+      //   await window.userStreamDoc.update(
+      //     userStreamContent,
+      //     {controllers: [window.did._id]}
+      //   )
+      //   await this.updateList()
+      //   this.isDataLoading = false
+      //   // todo update drawing state by window.userStreamDoc.content.nftRecords
+      // } catch (error) {
+      //   console.error('ERROR', error)
+      // }
     },
 
     async updateList () {
-      if (!this.isDemo) {
-        try {
-          if (window.userStreamDoc) {
-            const userStreamDoc = await ceramic.loadStream(window.userStreamDoc.id)
-            this.NFTNumber = null
-            this.fetchNFTs(userStreamDoc.content.nftRecords)
-          }
-        } catch (error) {
-          console.error(error)
-        }
-        return
-      }
-      this.fetchNFTs()
+      // if (!this.isDemo) {
+      //   try {
+      //     if (window.userStreamDoc) {
+      //       const userStreamDoc = await ceramic.loadStream(window.userStreamDoc.id)
+      //       this.NFTNumber = null
+      //       this.fetchNFTs(userStreamDoc.content.nftRecords)
+      //     }
+      //   } catch (error) {
+      //     console.error(error)
+      //   }
+      //   return
+      // }
+      // this.fetchNFTs()
     }
   }
 };
