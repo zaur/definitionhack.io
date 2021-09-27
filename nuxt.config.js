ba9fcfe0eb7a673bd436c340a0299620519ae090
techmeat <T5Kf8pY9w9yRVLs>
2021-09-27 17:57:12 +0300

    Update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ceramify-frontend',
+    title: 'Ceramify',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'NFT 2.0: interaction, composition and generative art' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
